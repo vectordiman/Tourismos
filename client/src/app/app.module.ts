e66cfbe93ad98c464fbd30375976f87a8d04800c
vectordiman <vector_diman@mail.ru>
2021-10-26 16:49:01 +0300

    fix: app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,14 +10,13 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {BsDropdownModule} from "ngx-bootstrap/dropdown";
 import { TextInputComponent } from './_forms/text-input/text-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
     AppComponent,
     NavComponent,
     RegisterComponent,
