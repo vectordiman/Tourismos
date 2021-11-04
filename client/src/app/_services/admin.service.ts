d7da0ad9d4a553967a1c510bfc4e09aa1d816808
Pluralment <Foolman.ru@yandex.ru>
2021-11-04 23:50:57 +0300

    add: shared module, jwt interceptor, admin service

---
+++ client/src/app/_services/admin.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { TourPackage } from '../_models/tour-package';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getUsersWithRoles() {
+    return this.http.get<Partial<User[]>>(this.baseUrl + "admin/user-roles");
+  }
+}
