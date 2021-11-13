cef97c65e7d89b1409a04ad0c4cf6910a82b9c5a
vectordiman <vector_diman@mail.ru>
2021-11-13 14:05:48 +0300

    add: user update dto

---
+++ API/DTOs/UserUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API.DTOs
+{
+    public class UserUpdateDto
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
