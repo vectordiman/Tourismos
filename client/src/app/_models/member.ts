6a27446cee082390b6fc90b9a3e2d884bce58e1b
vectordiman <vector_diman@mail.ru>
2021-11-13 16:44:16 +0300

    add: member, member service

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,9 @@
+export interface Member {
+  id:           number;
+  username:     string;
+  photoUrl:     string;
+  name:      string;
+  lastName:      string;
+  created:      Date;
+  lastActive:   Date;
+}
---
+++ client/src/app/_services/member.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import {AccountService} from "./account.service";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Member} from "../_models/member";
+import {User} from "../_models/user";
+import {map, take} from "rxjs/operators";
+import {of} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MemberService {
+  baseUrl = environment.apiUrl;
+  members: Member[] = [];
+  memberCache = new Map();
+  user!: User;
+
+
+  constructor(private http: HttpClient, private accountService: AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
+      this.user = user;
+    })
+  }
+
+  getMember(username: string | null) {
+    const member = [...this.memberCache.values()]
+      .reduce((arr, element) => arr.concat(element.result), [])
+      .find((member: Member) => member.username === username);
+
+    if(member) {
+      return of(member);
+    }
+
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    )
+  }
+
+  addMainPhoto(photoId: number) {
+    return this.http.post(this.baseUrl + 'users/add-main-photo', {});
+  }
+
+  deleteMainPhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'users/delete-main-photo', {});
+  }
+}
