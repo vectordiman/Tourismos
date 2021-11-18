dc2db9c28c9ea2d7e5db0310551c6005361d7bad
Pluralment <Foolman.ru@yandex.ru>
2021-11-18 18:46:42 +0300

    migration: PackageExpertAdded

---
+++ API/Data/Migrations/20211118090242_PackageExpertAdded.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class PackageExpertAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ExpertId",
+                table: "TourPackages",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TourPackages_ExpertId",
+                table: "TourPackages",
+                column: "ExpertId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TourPackages_AspNetUsers_ExpertId",
+                table: "TourPackages",
+                column: "ExpertId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TourPackages_AspNetUsers_ExpertId",
+                table: "TourPackages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TourPackages_ExpertId",
+                table: "TourPackages");
+
+            migrationBuilder.DropColumn(
+                name: "ExpertId",
+                table: "TourPackages");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUserRoles",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
